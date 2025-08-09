e2428e6ee2fef0f287660da86b001ac680dfd500
OweJeh <fobadu@st.ug.edu.gh>
2025-08-09 10:55:39 +0000

    interfaces done

---
+++ Interfaces/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace OOP_InterfaceDemo
+{
+    // Define interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Class Car implementing IMovable
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Class Bicycle implementing IMovable
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            // Call Move() on each
+            car.Move();
+            bicycle.Move();
+
+            // Keep console open
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
